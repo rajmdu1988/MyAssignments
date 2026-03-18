9e654c574ba24071982ce50664c67d03529570f3
rajmdu1988 <rajeshvnr1988@gmail.com>
2026-03-18 13:53:26 +0530

    Add files via upload
    week1-day1

---
+++ week1day1/dataTypes_March17Homework.js
@@ -0,0 +1,28 @@
+//string
+let username="Rajesh"
+console.log(username);
+console.log(typeof username);
+
+let organization="wagepoint"
+console.log(organization);
+console.log(typeof organization);
+
+//number
+let contactnumber=9786552543
+console.log(contactnumber);
+console.log(typeof contactnumber);
+
+//boolean
+let knowsAutomation=false
+console.log(knowsAutomation);
+console.log(typeof knowsAutomation);
+
+//undefined
+
+let usesPlaywright
+console.log(usesPlaywright);
+console.log(typeof usesPlaywright);
+
+
+
+
